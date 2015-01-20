14d484e716e26520cdd10d08f685fa9249c4205e
Odyssey <odyssey.in.ua@gmail.com>
2015-01-20 05:30:30 +0200

    Implementation of Dependency Injection design pattern. + JUnit test for App

---
+++ src/main/java/com/journaldev/java/dependencyinjection/test/MyMessageDITest.java
@@ -0,0 +1,29 @@
+package com.journaldev.java.dependencyinjection.test;
+
+import com.journaldev.java.dependencyinjection.consumer.Consumer;
+import com.journaldev.java.dependencyinjection.injector.EmailServiceInjector;
+import com.journaldev.java.dependencyinjection.injector.MessageServiceInjector;
+import com.journaldev.java.dependencyinjection.injector.SMSServiceInjector;
+
+public class MyMessageDITest {
+
+    public static void main(String[] args) {
+        String msg = "Hi Odyssey";
+        String email = "odyssey@abc.com";
+        String phone = "4088888888";
+
+        MessageServiceInjector injector = null;
+        Consumer app = null;
+
+        //Send email
+        injector = new EmailServiceInjector();
+        app = injector.getConsumer();
+        app.processMessages(msg, email);
+
+        //Send SMS
+        injector = new SMSServiceInjector();
+        app = injector.getConsumer();
+        app.processMessages(msg, phone);
+    }
+
+}
