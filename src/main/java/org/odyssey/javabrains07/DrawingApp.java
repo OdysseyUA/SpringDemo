23f3c6d7279bce78a47431f15a9761c3cf671431
Odyssey <odyssey.in.ua@gmail.com>
2015-01-27 00:49:22 +0200

    Object injection by ref

---
+++ src/main/java/org/odyssey/javabrains07/DrawingApp.java
@@ -0,0 +1,19 @@
+package org.odyssey.javabrains07;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Object injection (by ref)
+ */
+public class DrawingApp {
+
+	public static void main(String[] args) {
+
+        //BeanFactory context = new XmlBeanFactory(new FileSystemResource("src\\main\\resources\\javabrains\\spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("javabrains/spring.xml");
+        Triangle triangle = (Triangle)context.getBean("triangle");
+        triangle.draw();
+	}
+
+}
