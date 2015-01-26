a462d866b7fe5069cdf6b9c72766fcf13a1f8b66
Odyssey <odyssey.in.ua@gmail.com>
2015-01-27 00:32:15 +0200

    cleanup after reclone

--- src/main/java/org/odyssey/javabrains06/DrawingApp.java
+++ src/main/java/org/odyssey/javabrains06/DrawingApp.java
@@ -11,7 +11,7 @@ public class DrawingApp {
 	public static void main(String[] args) {
 
         //BeanFactory context = new XmlBeanFactory(new FileSystemResource("src\\main\\resources\\javabrains\\spring.xml"));
-		ApplicationContext context = new ClassPathXmlApplicationContext("org/odyssey/javabrains06/spring.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("javabrains/spring.xml");
         Triangle triangle = (Triangle)context.getBean("triangle");
         triangle.draw();
 	}
