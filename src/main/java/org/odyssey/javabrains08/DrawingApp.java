71a8e98af2a26ff4e40fe2c96482c0dbfcf44657
Odyssey <odyssey.in.ua@gmail.com>
2015-01-27 01:38:51 +0200

    Separated/inner bean injection alias-tag using. name-attribute using.

---
+++ src/main/java/org/odyssey/javabrains08/DrawingApp.java
@@ -0,0 +1,22 @@
+package org.odyssey.javabrains08;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Separated/inner bean injection
+ * alias-tag using.
+ * name-attribute using.
+ */
+public class DrawingApp {
+
+	public static void main(String[] args) {
+
+        //BeanFactory context = new XmlBeanFactory(new FileSystemResource("src\\main\\resources\\javabrains\\spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("javabrains/spring.xml");
+        Triangle triangle = (Triangle)context.getBean("triangle-alias");
+        //Triangle triangle = (Triangle)context.getBean("triangle-name");
+        triangle.draw();
+	}
+
+}
