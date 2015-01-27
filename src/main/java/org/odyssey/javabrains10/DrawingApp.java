cd28ad5f1385345a692669820fcda4d8816ad554
Odyssey <odyssey.in.ua@gmail.com>
2015-01-27 02:38:36 +0200

    * using aurowiring on bean level (without annotations)

--- src/main/java/org/odyssey/javabrains10/DrawingApp.java
+++ src/main/java/org/odyssey/javabrains10/DrawingApp.java
@@ -3,13 +3,17 @@ package org.odyssey.javabrains10;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+/***
+ * using aurowiring on bean level (without annotations)
+ */
 public class DrawingApp {
 
 	public static void main(String[] args) {
 
 	    //Triangle triangle = new Triangle();
         //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/journaldev/spring.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("javabrains/spring.xml");
         Triangle triangle = (Triangle)context.getBean("triangle");
         triangle.draw();
 	}
