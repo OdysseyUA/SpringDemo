79a39d0165cef20510bf07d9bac841690c35b99a
Odyssey <odyssey.in.ua@gmail.com>
2015-02-02 02:08:08 +0200

    Change in spring.xml and Circle class to applay autowiring.

--- src/main/java/org/odyssey/javabrains19/TheAutowiredAnnotation/Circle.java
+++ src/main/java/org/odyssey/javabrains19/TheAutowiredAnnotation/Circle.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
 
-	private Point center;	
+	private Point center;
 	
 	public void draw() {
 		System.out.println("Drawing Circle");
@@ -17,8 +17,10 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	@Autowired
-	@Qualifier("circleRelated")
+
+
+    @Autowired
+    @Qualifier("circleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
